7f4471dedf8306288827ea429485e3be40af1b70
suraj <sdevgan007@gmail.com>
2019-08-29 16:32:25 +0530

    Initial commit

---
+++ app/src/main/java/com/suraj/ciphet/HomeFragment.java
@@ -0,0 +1,69 @@
+package com.suraj.ciphet;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+
+import java.util.HashMap;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+    SliderLayout mDemoSlider;
+
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        mDemoSlider =  v.findViewById(R.id.slider);
+        HashMap<String,String> url_maps = new HashMap<>();
+        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+
+        for(String name:url_maps.keySet())
+        {
+            TextSliderView textSliderView = new TextSliderView(getContext());
+            textSliderView
+                    .description(name)
+                    .image(url_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit);
+
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",name);
+            mDemoSlider.addSlider(textSliderView);
+
+
+
+
+        }
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion); // this is the effect when slider changes Ny default there are 15 effects that we can choose
+
+
+
+
+
+        return v;
+    }
+
+
+}
---
+++ app/src/main/java/com/suraj/ciphet/MainActivity.java
@@ -0,0 +1,31 @@
+package com.suraj.ciphet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this,HomeActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
